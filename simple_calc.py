772582020753dd201b5c5c920df9cc558f605571
vaggab0nd <joeoreilly@gmail.com>
2026-01-06 10:46:06 +0000

    tic-tak-toe game

---
+++ simple_calc.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum_ = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+print("Sum:", sum_)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
